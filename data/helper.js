9e6c4d18dcc8cdfa02206f175da439da05678a93
Reyaad Rafeek <reyaad510@gmail.com>
2019-06-01 13:20:17 -0400

    Completed getting project by id and showing actions

--- data/helper.js
+++ data/helper.js
@@ -7,14 +7,29 @@ const getProjects = () => {
     return db('projects');
 }
 
-const getProject = id => {
-    return db('projects').where({ id: Number(id) });
+
+// Below making copy of initial project and then inserting project_id to clone to display the actions for that project
+const getProject = async id => {
+    const project = await db('projects').where({ id: Number(id) });
+    const actions = await db('actions').where({project_id: id});
+    const newProject = {...project[0]};
+    newProject.actions = actions;
+
+    if(newProject) {
+        return newProject
+    } else {
+        return null
+    }
 }
 
 
 const addProject = project => {
     return db('projects').insert(project);
 }
+
+
+
+
 
 const addAction = action => {
     return db('actions').insert(action);
--- routers/projects.js
+++ routers/projects.js
@@ -16,6 +16,22 @@ router.get('/', async(req, res) => {
 })
 
 
+// Get Project By ID
+
+router.get('/:id', async (req,res) => {
+    try {
+    const project = await db.getProject(req.params.id);
+    if(project) {
+        res.status(200).json(project)
+    } else {
+        res.status(404).json({ message: 'Couldn not find specific project :(' })
+    }
+}    catch(error) {
+    res.status(500).json({message: 'Error retreiving Project :('})
+}
+})
+
+
 // Add Project
 router.post('/', async(req,res) => {
     const project = req.body;
