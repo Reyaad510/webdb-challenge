c9aa3acf4def4093a1b4b332616641eeecbe321b
Reyaad Rafeek <reyaad510@gmail.com>
2019-06-01 12:48:17 -0400

    .Get functionality for Projects completed

--- routers/projects.js
+++ routers/projects.js
@@ -1,11 +1,19 @@
 const router = require('express').Router();
+const db = require('../data/helper.js');
 
 
 
+// /api/projects
+// Get Projects
 
-router.get('/', (req, res) => {
-    res.status(200).json({ message: "Projects!" });
-  });
+router.get('/', async(req, res) => {
+    try {
+        const projects = await db.getProjects();
+        res.status(200).json(projects);
+        } catch(error) {
+            res.status(500).json({message: 'Error retreiving projects! Sorry :('})
+    }
+})
 
 
 
